e25428f0447707e0bb35fd8aa3d728b6c7d92efa
Scoolnik <62171058+Scoolnik@users.noreply.github.com>
2022-11-17 18:52:44 +0500

    moved from css to scss added bg image animation on main screen

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     console.log(document.getElementById('test-id'))
-// })
+// let burger_button_mobile = document.getElementsByClassName("header_unwrap_button_mobile")[0];
+// let burger_button_tablet = document.getElementsByClassName("header_unwrap_button_tablet")[0];
+// let burger_close_button = document.getElementsByClassName("button_exit")[0];
+//
+// burger_button_mobile.addEventListener("click", showBurger);
+// burger_button_tablet.addEventListener("click", showBurger);
+// burger_close_button.addEventListener("click", hideBurger);
+//
+// function showBurger() {
+//     let element = document.getElementsByClassName("header_unwrapped_mobile")[0];
+//     element.classList.remove("hidden");
+// }
+//
+// function hideBurger(){
+//     let element = document.getElementsByClassName("header_unwrapped_mobile")[0];
+//     element.classList.add("hidden");
+// }
