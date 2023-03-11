7b42bf367ee899375a40fcc8d55098cc92b836c4
Scoolnik <62171058+Scoolnik@users.noreply.github.com>
2023-03-11 15:15:45 +0500

    completed task 13

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {Pizza, PizzaSize, PizzaStuffing, Topping} from './pizza.js'
+import {Pizza, PizzaSize, PizzaSizeVariant, PizzaStuffing, Topping} from './pizza.js'
 
 const stuffings = [
     new PizzaStuffing('Маргарита', 500, 300),
@@ -12,9 +12,79 @@ const toppings = [
     new Topping('Чеддер и пармезан',  150, 50),
 ]
 
+const stuffingsContainer = document.getElementById('stuffings-selector')
 
-const pizza1 = new Pizza(stuffings[0], PizzaSize.small);
-pizza1.addTopping(toppings[0])
-pizza1.addTopping(toppings[1])
-console.log(`price: ${pizza1.getPrice()}, calories: ${pizza1.getCalories()}`)
+function createCard(text) {
+    const element = document.createElement('div')
+    element.classList.add('btn', 'btn-light', 'card', 'text-center')
+    const img = document.createElement('image')
+    img.classList.add('card-img-top')
+    img.style.height = '100px'
+    const body = document.createElement('div')
+    const textBlock = document.createElement('p')
+    body.classList.add('card-body')
+    textBlock.innerText = text;
+    textBlock.classList.add('card-text')
+    body.append(textBlock)
+    element.append(img, body)
+    return element;
+}
 
+for (const stuffing of stuffings) {
+    const element = createCard(stuffing.name);
+    element.addEventListener('click', () => setPizzaStuffing(stuffing))
+    stuffingsContainer.append(element)
+}
+
+
+const sizesContainer = document.getElementById('size-selector')
+for (const size of Object.values(PizzaSize)) {
+    const element = document.createElement('label')
+    element.classList.add('btn', 'btn-light')
+    const inputElement = document.createElement('input')
+    inputElement.type = 'radio'
+    inputElement.classList.add('btn-check')
+    element.innerText = size.name;
+    element.append(inputElement)
+    sizesContainer.append(element)
+    element.addEventListener('click', () => setPizzaSize(size))
+}
+
+const toppingsContainer = document.getElementById('toppings-selector')
+for (const topping of toppings) {
+    const element = createCard(topping.name);
+    element.classList.add('shadow-sm')
+    element.setAttribute('data-bs-toggle', 'button')
+    element.addEventListener('click', ()=>togglePizzaTopping(topping, element.getAttribute('aria-pressed')=== 'true'))
+    toppingsContainer.append(element)
+}
+
+let currentPizza = new Pizza(stuffings[0], PizzaSize.small)
+onPizzaConfigChanged();
+
+function togglePizzaTopping(topping, isSelectedNow) {
+    if (isSelectedNow)
+        currentPizza.addTopping(topping)
+    else
+        currentPizza.removeTopping(topping)
+    onPizzaConfigChanged();
+}
+
+function setPizzaStuffing(stuffing) {
+    const toppings = currentPizza.toppings;
+    currentPizza = new Pizza(stuffing, currentPizza.size)
+    toppings.forEach(x=>currentPizza.addTopping(x))
+    onPizzaConfigChanged();
+}
+
+function setPizzaSize(size) {
+    const toppings = currentPizza.toppings;
+    currentPizza = new Pizza(currentPizza.stuffing, size)
+    toppings.forEach(x=>currentPizza.addTopping(x))
+    onPizzaConfigChanged();
+}
+
+function onPizzaConfigChanged() {
+    const pizzaInfoElement = document.getElementById('pizza-info')
+    pizzaInfoElement.innerText = `${currentPizza.getPrice()}Р (${currentPizza.getCalories()} Ккал)`
+}
